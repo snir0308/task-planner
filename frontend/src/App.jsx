402c5386c56d3e1c3205873ccbd0219948861426
snir0308 <sasitha.niranjana@sysco.com>
2026-09-15 15:39:35 +0530

    Changed from BrowserRouter to HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import RegistrationPage from "./components/RegistrationPage.jsx";
 import ViewOnlyBoard from "./components/ViewOnlyBoard.jsx";
 import TokenVerificationPage from "./components/TokenVerificationPage.jsx";
